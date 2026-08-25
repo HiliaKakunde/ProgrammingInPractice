7e8b6df2a970f36767356470a5d7dea9c92c2c15
Hilia Kakunde <224057235@nust.na>
2026-08-25 10:32:30 +0200

    Week 1 Municipal Financial Management System

--- main.c
+++ main.c
@@ -1,19 +1,29 @@
-#include <stdio.h> 
-int main() 
-{ 
-char municipality[50]; 
-char mayor[50]; 
-int population; 
-printf("Municipal Financial Management System\n\n"); 
-printf("Enter Municipality Name: "); 
-scanf("%49s", municipality);
-printf("Enter Mayor: "); 
-scanf("%49s", mayor); 
-printf("Enter Population: "); 
-scanf("%d", &population); 
-printf("\n---------------------------------\n"); 
-printf("Municipality : %s\n", municipality); 
-printf("Mayor        : %s\n", mayor); 
-printf("Population   : %d\n", population); 
-return 0; 
+#include <stdio.h>
+
+int main()
+{
+    // Practical Exercise 1
+    printf("Municipal Financial Management System\n");
+    printf("Welcome to Windhoek Municipality\n\n");
+
+    // Practical Exercise 2
+    char municipality[50];
+    char mayor[50];
+    int population;
+
+    printf("Enter Municipality Name: ");
+    scanf("%49s", municipality);
+
+    printf("Enter Mayor: ");
+    scanf("%49s", mayor);
+
+    printf("Enter Population: ");
+    scanf("%d", &population);
+
+    printf("\n-------------------------\n");
+    printf("Municipality : %s\n", municipality);
+    printf("Mayor        : %s\n", mayor);
+    printf("Population   : %d\n", population);
+
+    return 0;
 }
